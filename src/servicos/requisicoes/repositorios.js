da3ed3a1bf74c749c43355547f879988ef5d978c
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-09-18 00:29:54 -0300

    Buscando repositórios

---
+++ src/servicos/requisicoes/repositorios.js
@@ -0,0 +1,11 @@
+import api from '../api';
+
+export async function pegarRepositoriosDoUsuario(id) {
+    try {
+        const resultado = await api.get(`/repos?postId=${id}`);
+        return resultado.data;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+}
