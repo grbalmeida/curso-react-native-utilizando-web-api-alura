5c0f5bb8a5ddf8f098b2b7ba14063452348c2567
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-09-17 23:29:22 -0300

    Configurando a API no app

--- src/paginas/Principal/index.js
+++ src/paginas/Principal/index.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
 import { Text, View, Image, TouchableOpacity, TextInput, Alert, ScrollView } from 'react-native';
 import estilos from './estilos';
+import api from '../../servicos/api';
 
 export default function Principal({ navigation }) {
     const [nomeUsuario, setNomeUsuario] = useState('');
     const [usuario, setUsuario] = useState({});
+
+    function Busca() {
+        api.get('/users')
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.log('Erro');
+            });
+    }
 
     return (
         <ScrollView>
@@ -40,7 +51,7 @@ export default function Principal({ navigation }) {
                 />
 
                 <TouchableOpacity style={estilos.botao}>
-                    <Text style={estilos.textoBotao}>
+                    <Text style={estilos.textoBotao} onPress={Busca}>
                         Buscar
                     </Text>
                 </TouchableOpacity>
