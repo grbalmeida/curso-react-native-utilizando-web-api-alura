1aa7e893a22b273ab8d897db9fcfcaef6be2af16
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-09-18 00:42:23 -0300

    Editando os repositórios

--- src/paginas/InfoRepositorio/index.js
+++ src/paginas/InfoRepositorio/index.js
@@ -1,10 +1,27 @@
 import React, { useState } from 'react';
 import { Text, View, TouchableOpacity, TextInput, Alert } from 'react-native';
 import estilos from './estilos';
+import { salvarRepositorioDoUsuario } from '../../servicos/requisicoes/repositorios';
 
 export default function InfoRepositorio({ route, navigation }) {
-    const [nome, setNome] = useState('');
-    const [data, setData] = useState('');
+    const [nome, setNome] = useState(route.params.item.name);
+    const [data, setData] = useState(route.params.item.data);
+
+    async function salvar() {
+        const resultado = await salvarRepositorioDoUsuario(
+            route.params.item.postId,
+            nome,
+            data,
+            route.params.item.id
+        );
+
+        if (resultado === 'sucesso') {
+            Alert.alert('Repositório atualizado!');
+            navigation.goBack();
+        } else {
+            Alert.alert('Erro ao atualizar repositório');
+        }
+    }
 
     return (
         <View style={estilos.container}>
@@ -12,14 +29,19 @@ export default function InfoRepositorio({ route, navigation }) {
                 placeholder="Nome do repositório"
                 autoCapitalize="none"
                 style={estilos.entrada}
+                value={nome}
+                onChangeText={setNome}
             />
             <TextInput
                 placeholder="Data de criação"
                 autoCapitalize="none"
                 style={estilos.entrada}
+                value={data}
+                onChangeText={setData}
             />
             <TouchableOpacity 
                 style={estilos.botao} 
+                onPress={salvar}
             >
                 <Text style={estilos.textoBotao}>
                     Salvar
--- src/servicos/requisicoes/repositorios.js
+++ src/servicos/requisicoes/repositorios.js
@@ -4,8 +4,26 @@ export async function pegarRepositoriosDoUsuario(id) {
     try {
         const resultado = await api.get(`/repos?postId=${id}`);
         return resultado.data;
-    } catch (error) {
+    }
+    catch (error) {
         console.log(error);
         return [];
+    }
+}
+
+export async function salvarRepositorioDoUsuario(postId, nome, data, id) {
+    try {
+        await api.put(`/repos/${id}`, {
+            name: nome,
+            data: data,
+            postId: postId,
+            id: id
+        });
+
+        return 'sucesso';
+    }
+    catch (error) {
+        console.log(error);
+        return 'erro';
     }
 }
