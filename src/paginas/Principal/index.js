0cc7b99b43285634c9406de57703cf25fed0ec2a
Gilvan Ribeiro De Almeida <g.r.almeida@live.com>
2022-09-17 23:50:36 -0300

    Buscando usuários

--- src/paginas/Principal/index.js
+++ src/paginas/Principal/index.js
@@ -2,56 +2,64 @@ import React, { useState } from 'react';
 import { Text, View, Image, TouchableOpacity, TextInput, Alert, ScrollView } from 'react-native';
 import estilos from './estilos';
 import api from '../../servicos/api';
+import { buscaUsuario } from '../../servicos/requisicoes/usuarios';
 
 export default function Principal({ navigation }) {
     const [nomeUsuario, setNomeUsuario] = useState('');
     const [usuario, setUsuario] = useState({});
 
-    function Busca() {
-        api.get('/users')
-            .then(response => {
-                console.log(response.data);
-            })
-            .catch(error => {
-                console.log('Erro');
-            });
+    async function busca() {
+        const resultado = await buscaUsuario(nomeUsuario);
+        
+        setNomeUsuario('');
+
+        if (resultado) {
+            setUsuario(resultado);
+        } else {
+            Alert.alert('Usuário não encontrado');
+            setUsuario({});
+        }
     }
 
     return (
         <ScrollView>
             <View style={estilos.container}>
-                <>
-                    <View style={estilos.fundo} />
-                    <View style={estilos.imagemArea}>
-                        <Image source={{ uri: 'https://avatars.githubusercontent.com/u/54721131?v=4' }} style={estilos.imagem} />
-                    </View>
-                    <Text style={estilos.textoNome}>Nome do usuario</Text>
-                    <Text style={estilos.textoEmail}>Email do usuario</Text>
-                    <View style={estilos.seguidoresArea}>
-                        <View style={estilos.seguidores}>
-                            <Text style={estilos.seguidoresNumero}>30</Text>
-                            <Text style={estilos.seguidoresTexto}>Seguidores</Text>
+                {usuario?.login &&
+                    <>
+                        <View style={estilos.fundo} />
+                        <View style={estilos.imagemArea}>
+                            <Image source={{ uri: usuario.avatar_url }} style={estilos.imagem} />
                         </View>
-                        <View style={estilos.seguidores}>
-                            <Text style={estilos.seguidoresNumero}>40</Text>
-                            <Text style={estilos.seguidoresTexto}>Seguindo</Text>
+                        <Text style={estilos.textoNome}>{usuario.name}</Text>
+                        <Text style={estilos.textoEmail}>{usuario.email}</Text>
+                        <View style={estilos.seguidoresArea}>
+                            <View style={estilos.seguidores}>
+                                <Text style={estilos.seguidoresNumero}>{usuario.followers}</Text>
+                                <Text style={estilos.seguidoresTexto}>Seguidores</Text>
+                            </View>
+                            <View style={estilos.seguidores}>
+                                <Text style={estilos.seguidoresNumero}>{usuario.following}</Text>
+                                <Text style={estilos.seguidoresTexto}>Seguindo</Text>
+                            </View>
                         </View>
-                    </View>
-                    <TouchableOpacity onPress={() => navigation.navigate('Repositorios')}>
-                        <Text style={estilos.repositorios}>
-                            Ver os repositórios
-                        </Text>
-                    </TouchableOpacity>
-                </>
+                        <TouchableOpacity onPress={() => navigation.navigate('Repositorios')}>
+                            <Text style={estilos.repositorios}>
+                                Ver os repositórios
+                            </Text>
+                        </TouchableOpacity>
+                    </>
+                }
 
                 <TextInput
                     placeholder="Busque por um usuário"
                     autoCapitalize="none"
                     style={estilos.entrada}
+                    value={nomeUsuario}
+                    onChangeText={setNomeUsuario}
                 />
 
-                <TouchableOpacity style={estilos.botao}>
-                    <Text style={estilos.textoBotao} onPress={Busca}>
+                <TouchableOpacity style={estilos.botao} onPress={busca}>
+                    <Text style={estilos.textoBotao}>
                         Buscar
                     </Text>
                 </TouchableOpacity>
